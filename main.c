b22b73b1b2dbac30c7cb69b6f4daf6e0a65a8893
lulm <lionelulm67@gmail.com>
2024-01-26 08:53:28 +0100

    updated ft_printf

--- main.c
+++ main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lionelulm <lionelulm@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lulm <lulm@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/26 03:00:38 by lionelulm         #+#    #+#             */
-/*   Updated: 2024/01/26 03:00:42 by lionelulm        ###   ########.fr       */
+/*   Updated: 2024/01/26 08:43:05 by lulm             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,38 @@
 
 int main()
 {
-	char	c = 'a';
-	int		intd = 111111;
-	int		inti = 222222;
-	char	str[] = "Coucou, c'est moi !";
+	char			c = 'a';
+	int				intd = 2147483647;
+	int				inti = 2147483647;
+	char			str[] = "Coucou, c'est moi !";
+	int				*ptr = &intd;
+	unsigned int	hexa = 255;
+	unsigned int	dec = 333333;
 
 	ft_printf("Char: %c\n", c);
 	ft_printf("Int d: %d\n", intd);
 	ft_printf("Int i: %i\n", inti);
 	ft_printf("String: %s\n", str);
-	ft_printf("Tout: %c, %d, %i, %s, %%", c, intd, inti, str);
+	ft_printf("Pointer: %p\n", ptr);
+	ft_printf("Unsigned Decimal: %u\n", dec);
+	ft_printf("Lower Hexa: %x\n", hexa);
+	ft_printf("Upper Hexa: %X\n", hexa);
+	ft_printf("Tout: %c, %d, %i, %s, %p, %u, %x, %X, %%,\n\n", c, intd, inti, str, ptr, dec, hexa, hexa);
+
+	printf("Char: %c\n", c);
+	printf("Int d: %d\n", intd);
+	printf("Int i: %i\n", inti);
+	printf("String: %s\n", str);
+	printf("Pointer: %p\n", ptr);
+	printf("Unsigned Decimal: %u\n", dec);
+	printf("Lower Hexa: %x\n", hexa);
+	printf("Upper Hexa: %X\n", hexa);
+	printf("Tout: %c, %d, %i, %s, %p, %u, %x, %X, %%,\n", c, intd, inti, str, ptr, dec, hexa, hexa);
 
 	return (0);
 }
 
 //à tester :
 //voir si printf normal peut avoir plusieurs char à %c, voir si c'est diff.
-//tester %p, %i (voir si diff à %d), %u, %x, %X, %%
+//tester %u, %x, %X, %%
 //réparer i et d, ils ne devraient pas faire 97
